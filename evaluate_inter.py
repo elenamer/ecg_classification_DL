b01dd5b55c14d1d95e3649afcffa614ff70d1e13
Elena Merdjanovska <elena-carica@elenam-laptop>
2021-04-28 00:59:16 +0200

    add evaluate inter script

---
+++ evaluate_inter.py
@@ -0,0 +1,90 @@
+from models.resnet import ResNet
+from evaluation.metrics import F1Metric
+from datasets.ptbxldataset import PTBXLDataset
+from datasets.arr10000dataset import Arr10000Dataset
+#from datasets.datagenerator import DataGenerator
+from datasets.mitbihardataset import MITBIHARDataset
+import tensorflow as tf
+import os
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import pickle
+import numpy as np
+
+seed = 102
+
+classes = ["N", "S", "V", "F", "Q"]
+
+#classes = "rhythm"
+
+num_classes = 5
+
+db_name = "mitdb"
+choice = "static"
+eval_p = "inter"
+
+dat = MITBIHARDataset(db_name)
+
+## Warning: for now balance = True and False are treated the same and saved to same files (i.e. overwritten)
+dat.generate_train_set(eval_p,choice,True)
+dat.generate_test_set(eval_p,choice,True)
+
+
+train = dat.load_dataset(eval_p,choice, 'train', '' )
+test = dat.load_dataset(eval_p,choice, 'test', '' )
+
+
+if len(dataset) != 2:
+    print("generate crossval splits")
+    # we need to do random n-crossval splits for val and test
+
+#skf = StratifiedKFold(n_splits=10)
+#for i, (train_val_ind, test_ind) in enumerate(skf.split(dataset[0],dataset[1])):
+
+# experiments/mitdb/static/specificpatient/201/models/"
+exp_path = "experiments"+os.sep+db_name+os.sep+choice+os.sep+eval_p+"patient"#+os.sep+str(i)
+if not os.path.exists(exp_path):
+        os.makedirs(exp_path)
+        os.mkdir(exp_path+os.sep+"models")
+        os.mkdir(exp_path+os.sep+"results")
+
+X_test = test[0]
+y_test = test[1]
+
+
+X_train, X_val, y_train, y_val = train_test_split(train[0], train[1], random_state=seed, shuffle=True, test_size=0.15)
+
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=len(classes))
+y_val = tf.keras.utils.to_categorical(y_val, num_classes=len(classes))
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=len(classes))
+
+model = ResNet(num_outputs=num_classes, blocks=[1,1], filters=[32, 64], kernel_size=[15,15])
+
+inputs = tf.keras.layers.Input((200,1,), dtype='float32')
+m1 = tf.keras.Model(inputs=inputs, outputs=model.call(inputs))
+
+opt = tf.keras.optimizers.Adam(lr=0.0001)
+
+m1.compile(optimizer=opt,
+            #tf.keras.optimizers.Adam(beta_1=0.9, beta_2=0.98, epsilon=1e-9),
+            #loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            loss='categorical_crossentropy',
+            metrics='acc')
+
+es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=6)
+log_f1 = F1Metric(train=(X_train, y_train), validation=(X_val, y_val), path=exp_path+os.sep+"models")
+
+
+m1.fit(x=X_train,y=y_train, validation_data = (X_val, y_val), callbacks = [es, log_f1], epochs = 100)
+
+y_pred = m1.predict(X_test)
+cm = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1),labels=range(num_classes))
+output = open(exp_path+os.sep+'CM_test.pkl', 'wb')
+pickle.dump(cm, output)
+output.close()
+
+y_pred = m1.predict(X_val)
+cm = confusion_matrix(y_val.argmax(axis=1), y_pred.argmax(axis=1),labels=range(num_classes))
+output = open(exp_path+os.sep+'CM_val.pkl', 'wb')
+pickle.dump(cm, output)
+output.close()
---
+++ summarize_results.py
@@ -0,0 +1,190 @@
+import os
+import argparse
+import numpy as np
+import pandas as pd
+import pickle
+
+'''
+
+ACC
+PPV - Precision
+TPR - Sen - Recall 
+TNR - Spe
+F1 
+
+'''
+
+parser = argparse.ArgumentParser(description='Choose root path of all result files')
+parser.add_argument('--path', type=str,
+                    help='path to results')
+args = parser.parse_args()
+results_path = args.path
+
+classes = ["N","S","V","F","Q"]
+
+metrics = ["ACC","PPV","TPR","F1","support"]
+experiments = ["specific"]
+patients = {"specific":[201,203,205,207,208,209,215,220,223,230,200,202,210,212,213,214,219,221,222,228,231,232,233,234],
+"inter": ['1'],
+"intra": [0,1,2,3,4,5,6,7,8,9]
+}
+segmentations = ["static"]#,"incartdb","svdb","edb"]#,"incartdb","svdb","edb"]#,'birnn'] # "birnn", "deeptransf"
+models = sorted(os.listdir(results_path))
+
+models = [
+"test",
+"val"
+]
+
+print(models)
+
+def evaluate_metrics(confusion_matrix):
+    # https://stackoverflow.com/questions/31324218/scikit-learn-how-to-obtain-true-positive-true-negative-false-positive-and-fal
+    # Sensitivity, hit rate, recall, or true positive rate
+    FP = confusion_matrix.sum(axis=0) - np.diag(confusion_matrix)
+    FN = confusion_matrix.sum(axis=1) - np.diag(confusion_matrix)
+    TP = np.diag(confusion_matrix)
+    TN = confusion_matrix.sum() - (FP + FN + TP)
+    support = confusion_matrix.sum(axis=1)
+    eps = 0.0000001
+    TPR = TP / (TP + FN + eps)
+    # Specificity or true negative rate
+    TNR = TN / (TN + FP + eps)
+    # Precision or positive predictive value
+    PPV = TP / (TP + FP + eps)
+    # Negative predictive value
+    NPV = TN / (TN + FN + eps)
+    # Fall out or false positive rate
+    FPR = FP / (FP + TN + eps)
+    # False negative rate
+    FNR = FN / (TP + FN + eps)
+    # False discovery rate
+    FDR = FP / (TP + FP + eps)
+    # F1 score
+    F1 = 2 * PPV * TPR / (PPV + TPR + eps)
+    # Overall accuracy
+    ACC = (TP + TN) / (TP + FP + FN + TN)
+    # ACC_micro = (sum(TP) + sum(TN)) / (sum(TP) + sum(FP) + sum(FN) + sum(TN))
+
+    ACC_macro = np.mean(
+        ACC)  # to get a sense of effectiveness of our method on the small classes we computed this average (macro-average)
+    results_dict = {"ACC_macro":ACC_macro, "ACC":ACC, "TPR":TPR, "TNR":TNR, "PPV":PPV}
+    return ACC, TPR, TNR, PPV, F1, support
+
+'''
+for root, dirs, files in os.walk(results_path, topdown=False):
+   for name in files:
+       if name.endswith(".csv") and "test" not in name:
+            print(name)
+            try:
+                arr = np.loadtxt(os.path.join(root, name))
+                # if it is only numpy then it hasn't been processed yet
+            except:
+                # if numpy load fails, then it has been processed and do nothing
+                continue
+            print(arr)
+            print(os.path.join(root, name))
+            if "ACC_macro" in name:
+                df = pd.DataFrame(arr, columns=["average"], index=np.arange(1,arr.shape[0]+1))
+            else:
+                df = pd.DataFrame(arr, columns=classes, index=np.arange(1,arr.shape[0]+1))
+            df.loc["mean"]=df.mean()
+            df.loc["std"]=df.std()
+            print(df)
+            df.to_csv(os.path.join(root, name), float_format="%.5f")
+'''
+
+segmentations_ind = segmentations*len(models)
+models_ind = np.repeat(models, len(segmentations))
+stats = ["mean"]#,"std","min","max"]
+
+classes_and_all = ["All"] + classes
+classes_ind = np.repeat(classes_and_all, len(metrics)*len(stats))
+metrics_ind = np.repeat(metrics, len(stats)) 
+metrics_ind = [str(s) for s in metrics_ind]
+metrics_ind = metrics_ind * len(classes_and_all)
+stats_ind = stats * len(metrics) * len(classes_and_all)
+#
+print(len(stats_ind))
+print(len(classes_ind))
+print(len(metrics_ind))
+results_df1 = pd.DataFrame(columns = [classes_ind, metrics_ind, stats_ind], index=[models_ind, segmentations_ind] )
+results_df1 = results_df1.fillna('/')
+results_df2 = pd.DataFrame(columns = [classes_ind, metrics_ind, stats_ind], index=[models_ind, segmentations_ind] )
+results_df2 = results_df2.fillna('/')
+results_df3 = pd.DataFrame(columns = [classes_ind, metrics_ind, stats_ind], index=[models_ind, segmentations_ind] )
+results_df3 = results_df2.fillna('/')
+results = {"intra": results_df1, "inter": results_df2, "specific": results_df3}
+
+print("BREAK")
+for model in models:
+    for segmentation_method in segmentations:
+        for evaluation_method in experiments:
+            skip=False
+            ACC=[]
+            TPR=[]
+            TNR=[]
+            PPV=[]
+            F1=[]
+            support=[]
+            sum_arr = np.zeros((len(classes),len(classes)))
+            for patient in patients[evaluation_method]:
+                patient = str(patient)
+                try:
+                    cm_path = os.path.join(results_path, segmentation_method, evaluation_method+"patient" , patient, "CM_"+model+".pkl")
+                    print(cm_path)
+                    with open(cm_path, "rb") as f:
+                        #print(os.path.join(results_path, model, "results-"+evaluation_method+"patient"+patient, "CM_"+segmentation_method+".pkl"))
+                        arr = pickle.load(f)
+                        print(arr)
+                        if arr[0] is None:
+                            continue
+                except OSError as e:
+                    skip=True
+                    print(cm_path+" not found")
+                    continue
+
+                sum_arr+=arr    
+            print(sum_arr)               
+            acc, sensitivity, specificity, ppv, f1, sup = evaluate_metrics(sum_arr)
+            # ACC.append(acc)
+            # TPR.append(sensitivity)
+            # TNR.append(specificity)
+            # PPV.append(ppv)
+            # F1.append(f1)
+            # support.append(sup)
+            if skip:
+                continue
+            metric_results = {"ACC":acc, "TPR": sensitivity, "PPV":ppv,"F1":f1, "support":sup}
+            #print(ACC)
+            for metric in metrics:
+            #print(name)
+                #arr=np.array(metric_results[metric])
+                df = pd.DataFrame(arr, columns=classes, index=np.arange(1,arr.shape[0]+1))
+                #df1 = df.copy()
+                df.loc["mean"]=metric_results[metric]#df1.mean()
+                #df.loc["std"]=df1.std()  
+                #df.loc["min"]=df1.min()
+                #df.loc["max"]=df1.max() 
+                #print(df)
+                for cl in classes:   
+                    mean = df.loc['mean',cl]
+                    #std = df.loc['std',cl]
+                    #mmin = df.loc['min',cl]
+                    #mmax = df.loc['max',cl]
+                    #print(mean)
+                    results[evaluation_method].loc[(model, segmentation_method),(cl, metric,"mean")] = mean
+                    #results[evaluation_method].loc[(model, segmentation_method),(cl, metric,"std")] = std
+                    #results[evaluation_method].loc[(model, segmentation_method),(cl, metric,"min")] = mmin
+                    #results[evaluation_method].loc[(model, segmentation_method),(cl, metric,"max")] = mmax
+                results[evaluation_method].loc[(model, segmentation_method),("All", metric,"mean")] = np.mean(df.loc['mean'], axis=0)
+                #results[evaluation_method].loc[(model, segmentation_method),("All", metric,"std")] = np.mean(df.loc['std'], axis=0)
+                #results[evaluation_method].loc[(model, segmentation_method),("All", metric,"min")] = np.mean(df.loc['min'], axis=0)
+                #results[evaluation_method].loc[(model, segmentation_method),("All", metric,"max")] = np.mean(df.loc['max'], axis=0)
+for evaluation_method in experiments:
+    print(evaluation_method)
+    results[evaluation_method] = results[evaluation_method].round(5)
+    print(results[evaluation_method])
+    results[evaluation_method].to_csv(os.path.join(results_path, "summary_"+evaluation_method+".csv"))
+# remove macro file
+# add intra- and inter- in folder name
