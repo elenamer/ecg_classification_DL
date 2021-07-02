cfec2bbe58724a98992320de3e1f5dbfa8d68d1f
elenamer <elenamerdzanovska@yahoo.com>
2021-07-02 17:28:43 +0200

    implement windowing

--- models/model.py
+++ models/model.py
@@ -9,7 +9,7 @@ class Classifier(tf.keras.Model):
         super(Classifier, self).__init__()
         self.num_classes = n_classes
         self.learning_rate=learning_rate
-        self.input_size = input_seconds * frequency
+        self.input_size = int(input_seconds * frequency)
         self.model = model#tf.keras.Model(inputs=inputs, outputs=model.call(inputs))
         out_act = 'sigmoid' if n_classes == 1 else 'softmax' # change this for multi-label
         self.classifier = tf.keras.layers.Dense(n_classes, out_act)
@@ -35,19 +35,19 @@ class Classifier(tf.keras.Model):
         x = self.classifier(x)
         return x
     
-    def fit(self, X, y, validation_data):
+    def fit(self, x, y, validation_data):
 
         es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
-        log_f1 = F1Metric(train=(X, y), validation=validation_data, path=self.path+os.sep+"models")
+        log_f1 = F1Metric(train=(x, y), validation=validation_data, path=self.path+os.sep+"models")
         wandb_cb = WandbCallback(save_weights_only=True)
 
-        X, y, idxs = process(x=X,labels=y,input_size=self.input_size, window=True)
+        x, y, idxs = process(X=x,labels=y,input_size=self.input_size, window=True)
 
-        X_val, y_val, idxs = process(x = validation_data[0], labels=validation_data[1],input_size=256, window=True)
-        super(Classifier, self).fit(X, y, validation_data = (X_val, y_val), callbacks = [es, log_f1, wandb_cb], epochs = self.epochs)
+        X_val, y_val, idxs = process(X = validation_data[0], labels=validation_data[1],input_size=self.input_size, window=True)
+        super(Classifier, self).fit(x, y, validation_data = (X_val, y_val), callbacks = [es, log_f1, wandb_cb], epochs = self.epochs)
 
-    def predict(self, X, y):
-        X, idxs = process((X,),input_size=self.input_size, window=True)
+    def predict(self, X):
+        X, idxs = process(X=X,input_size=self.input_size, window=True)
 
         preds = super().predict(X) # always on window-level
         agg_preds = aggregate_labels(preds, idxs)
@@ -59,6 +59,7 @@ class Classifier(tf.keras.Model):
 
 
 def aggregate_labels(preds, idmap):
+    aggregate_fn = np.mean
     if(idmap is not None and len(idmap)!=len(np.unique(idmap))):
         print("aggregating predictions...")
         preds_aggregated = []
@@ -72,13 +73,33 @@ def pad(sig, size):
     return np.pad(np.array(sig), (0, size - len(sig)))
 
 def process(X, input_size, labels=None, window=False):
+    overlap = 0.5
+    print("windowing")
     if window==True:
-        for sig, idx in enumerate()
-
-    else:
-        X = [pad(sig[:input_size], input_size) for sig in X]
+        new_data = []
+        new_labels = []
+        idmap = []
+        for ind, sig in enumerate(X):
+            #print(sig)
+            step = int(input_size*overlap)
+            nrows = ((len(sig)-input_size)//step)+1
+            print(nrows)
+            windows = sig[step*np.arange(nrows)[:,None] + np.arange(input_size)]
+            #print(windows)
+            new_data.extend(windows.tolist())
+            if labels is not None:
+                new_labels.extend([labels[ind]] * nrows)
+            idmap.extend([ind] * nrows)
+            #print(idmap)
+    new_data = tf.keras.preprocessing.sequence.pad_sequences(new_data, maxlen=input_size, dtype="float32", padding="post", truncating="post", value=0.0)
+    if new_data.ndim == 2:
+        new_data = new_data[:,:,None]
+    print(new_data.shape)
+    if labels is None:
+        return new_data, idmap
         # just crop/pad if needed
         # convert to numpy array
 
-    X=np.array(X)
-    return X[:,:,None]
+    new_labels = np.array(new_labels)
+    print(new_labels.shape)
+    return new_data, new_labels, idmap
