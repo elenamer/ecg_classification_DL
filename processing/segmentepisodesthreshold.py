51791b2d1797b0dd0173f502e256742931d932ae
elenamer <elenamerdzanovska@yahoo.com>
2021-10-20 18:24:43 +0200

    implement both episode segmentation variations

--- processing/segmentepisodesthreshold.py
+++ processing/segmentepisodesthreshold.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
-import os
 
 def normalize(data):
     data = np.nan_to_num(data)  # removing NaNs and Infs
@@ -22,10 +21,57 @@ def normalize(data):
         print("this again still")
     return data
 
-class SegmentEpisodes():
+def func(row):
+    labls = []
+    labls.append(row.ep_label)
+    if sum(row.labels_mlb) != 0:
+        #print(row.labels_mlb)
+        l = np.array(row.labels_mlb).argmax()
+        #print(lls)
+        #print(l)
+        labls.append(l)
+    #print(labls)
+    #print(labls)
+    return labls
+
+def get_episode_label(labels):
+    all_labels = []
+    for ind, row in labels.iterrows():
+        l = func(row)
+        all_labels.extend(l)
+    values, counts = np.unique(np.array(all_labels), return_counts = True)
+    to_drop = np.where(values == '')[0]
+    #print(values)
+    #print(counts)
+    values = np.delete(values, to_drop)
+    counts = np.delete(counts, to_drop)
+    if counts.size > 0:
+        print(values)
+        # if counts.size > 1:
+        #     to_drop = np.where(values == '0')[0]
+        #     values = np.delete(values, to_drop)
+        #     counts = np.delete(counts, to_drop)
+        # chosen_label_index = counts.argmax()
+        # chosen_label = values[chosen_label_index]
+        # print(chosen_label)
+        # # if str(chosen_label) != '0' and str(chosen_label) != '':
+        # #     print(values)
+        # #     print(counts)
+    else:
+        chosen_label = ''
+        return [chosen_label]
+    #if counts[chosen_label_index] < 4:
+    #    return ''
+    #all_labels.append(labels.ep_label.values)
+    #all_labels.append(labels.ep_label.values.argmax(axis=1))
+    return values
+
+
+
+class SegmentEpisodesThreshold():
     def __init__(self, input_size, fs):
         self.input_size = input_size
-        self.name = "segmentepisodes"
+        self.name = "segmentepisodesthreshold"
         self.fs = fs
 
     def aggregate_labels(self, preds, idmap=None):
@@ -46,20 +92,22 @@ class SegmentEpisodes():
             return np.array(preds)
 
     def segment_episodes(self, choice, signal, labels_orig, start_minute, end_minute):
-        # here start minute is basically start sample
+        '''
         
+        Main idea: have realistic windows. for real. not ''edited'' according to class in any way.
+        
+        '''
+        
+        # here start minute is basically start sample
 
-        # ? I don't know for now
-        # N_SAMPLES_BEFORE_R_dynamic=int(fs/4.5)
         print(labels_orig.index)
 
         start_sample = start_minute #int(start_minute * fs * 60)
         start_ind = np.argmax(labels_orig.index >= start_sample)
         if end_minute == -1:
-            end_ind = len(signal)
-        else: 
+            end_sample = len(signal)
+        else:
             end_sample = end_minute #int(end_minute * fs * 60)
-            end_ind = np.argmax(labels_orig.index >= end_sample)
 
         # print("Start index:")
         # print(start_ind)
@@ -69,20 +117,21 @@ class SegmentEpisodes():
         data=[]
         all_labls = []
         labels = pd.DataFrame(labels_orig)
+
         indices = np.where(np.array(labels.orig_label) == '+')[0]
         #print(labels.index[indices])
 
+
         for ind, rpeak_ind in enumerate(labels.index[indices]):
 
+            current_ep = labels.loc[rpeak_ind].ep_label
+
+            print(current_ep)
+            # if current_ep == '':
+            #     print("VERYYY IMPORTANT")
             #print(ind, rpeak_ind)
             rPeak_start = rpeak_ind
-            #print(labels.loc[ind])
-            label=labels.loc[rPeak_start]
-            #print(label)
-            if label.ep_label == '':
-                continue
-            #print(label)
-
+            
             if len(indices) > ind+1:
                 # print("END DEBUG:")
                 # print(ind+1)
@@ -91,38 +140,59 @@ class SegmentEpisodes():
             else:
                 rPeak_end = len(signal) - 1
 
-            print(rPeak_end)
-            print(rPeak_start)
-            print(self.input_size)
-            if rPeak_end - rPeak_start > self.input_size:
-               rPeak_end = rPeak_start + ( self.input_size)
-            print(rPeak_end)
+            for ind, label in labels.loc[rPeak_start:rPeak_end].iterrows():#range(rPeak_start,rPeak_end):
+                #label = labels.loc[ind]
 
+                if label.ep_label == '':
+                    label.ep_label = current_ep
+            #if current_ep == '':
+                #print(labels.loc[rPeak_start:rPeak_end])
+
+            #print(labels.loc[rPeak_start:rPeak_end])
             #print(label)
-            #print(rPeak)
 
-            sig = signal[rPeak_start:rPeak_end]
-            # print("STart: "+str(rPeak_start))
-            # print("End: "+str(rPeak_end))
+        #input()
+
+        for window_start in range(start_sample, end_sample, self.input_size):
+            
+            window_end = window_start+self.input_size
+            #print(self.input_size / 360)
+            start_ind = np.argmax(labels_orig.index >= window_start)
+
+
+            '''
+            problematic end ind
+            
+            '''
+            if np.argmax(labels_orig.index >= window_end) != 0:
+                end_ind = np.argmax(labels_orig.index >= window_end)
+            else:
+                end_ind = len(signal)
+
+
+
+            #print("window: "+str(window_start)+"   "+str(window_end))
+            #print("index: "+str(start_ind)+"   "+str(end_ind))
+            episode_labels=labels.iloc[start_ind:end_ind]
+            #print(labels.iloc[start_ind:end_ind])
+            label = get_episode_label(episode_labels)
+
+            if label[0] == '':
+                continue
+            
+            #print(label)
+
+            sig = signal[window_start:window_end]
 
             if np.std(sig)==0:
                 print("this happened")
                 continue
             #data.append(sig)
             data.append(normalize(sig))
-            print(label.ep_label)
-            all_labls.append(int(label.ep_label))
-            
-
-            # uncomment for episodes visualization
-
-            # if int(label.ep_label) != 0:
-            #     fig, ax = plt.subplots()               
-            #     ax.plot(normalize(sig))
-            #     ax.annotate(int(label.ep_label), xy=(0, sig[0]), xycoords='data')
-            #     plt.show()
-
+            #print(label)
+            all_labls.extend(label)
             #print(all_labls)
+            #plt.plot(normalize(sig))
 
         # print(skipped)
         # print(len(data))
@@ -162,30 +232,16 @@ class SegmentEpisodes():
             self.groupmap.extend([ind]*len(beats))
             #print(full_labels)
 
-        ## uncomment for duration distributions per class
+        lengths = []
+        for episode in full_data:
+            lengths.append(len(episode)/ self.fs)
 
-        # distrs_splits = {}
-        # for ind, episode in enumerate(full_data):
-
-        #     key = int(full_labels[ind])
-            
-        #     if key in distrs_splits:
-        #         distrs_splits[key].append(len(episode)/ self.fs)
-        #     else:
-        #         distrs_splits[key] = []
-        # for ind, key in enumerate(distrs_splits.keys()):
-        #     ax = sns.kdeplot(distrs_splits[key], shade=True, label = key)
-        #     if ind>len(list(distrs_splits.keys()))-2:
-        #         break
-        #     for key2 in list(distrs_splits.keys())[ind+1:]:
-        #         if len(distrs_splits[key2]) == 0:
-        #             continue
-        #         # text_file.write("pair: "+str(key)+" "+str(key2)+"\n")
-        #         # text_file.write(str(scipy.stats.ks_2samp(distrs_splits[key],distrs_splits[key2]))+"\n")
-
-        # ax.legend()
-        # ax.set_title("Episode duration distribution for "+self.name+" Dataset")
-        # plt.show()
+        ax = sns.histplot(full_labels, stat='probability')
+        ax.set_title("Episode label distribution for "+self.name+" Dataset")
+        #plt.show()
+        for container in ax.containers:
+            ax.bar_label(container)
+        plt.savefig("./"+self.name+"_ep_distr_mlb.png", dpi=300)
 
         self.idmap = np.arange(len(full_data))
         print("after processing")
