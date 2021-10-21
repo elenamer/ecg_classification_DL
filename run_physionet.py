3e58c67d2afac1df5f8da67fcb1fb805bfe9a929
elenamer <elenamerdzanovska@yahoo.com>
2021-10-21 11:56:47 +0200

    prepare run script for aami task (beats vs episodes)

--- run_physionet.py
+++ run_physionet.py
@@ -19,9 +19,16 @@ from processing.slidingwindow import SlidingWindow
 from datasets.ptbxldataset import PTBXLDataset
 from evaluation.experiment import Experiment 
 
-combinations_dict = {
-    "form" : [(MITBIHARDataset), (INCARTDataset), (MITBIHSVDataset), (LongTermAFDataset)],
+combinations_dict_episodes = {
     "aami": [(MITBIHARDataset), (INCARTDataset), (MITBIHSVDataset), (LongTermAFDataset)]
+    #"rhythm" : [(MITBIHARDataset, 10, 10)]#, (Arr10000Dataset, 10, 10)],
+    #'cinc2017' : [(CPSC2018Dataset, 30, 60), (CincChallenge2017Dataset, 10, 30), (PTBXLDataset, 10, 10), (Arr10000Dataset, 10, 10)],
+    #"cpsc2018" : [(CPSC2018Dataset, 30, 60), (PTBXLDataset, 10, 10)]   
+}
+
+
+combinations_dict_beats = {
+    "aami": [(LongTermAFDataset)]
     #"rhythm" : [(MITBIHARDataset, 10, 10)]#, (Arr10000Dataset, 10, 10)],
     #'cinc2017' : [(CPSC2018Dataset, 30, 60), (CincChallenge2017Dataset, 10, 30), (PTBXLDataset, 10, 10), (Arr10000Dataset, 10, 10)],
     #"cpsc2018" : [(CPSC2018Dataset, 30, 60), (PTBXLDataset, 10, 10)]   
@@ -30,10 +37,52 @@ combinations_dict = {
 # if physionet dataset & rhythm task => segment episodes ( Transform/SlidingWindow and flag=true)
 # if physionet dataset & form task => segment beats (SegmentBeats and flag=false)
 
-for model, sec, freq in [(CNN, 0.72, 360),(CPSCWinnerNet, 0.72, 500), (ResNet, 0.72, 250), (CNN, 10, 360),(RTACNN, 0.72, 300)]: #(CNN, 10, 360), (RTACNN, 30, 300), (CPSCWinnerNet, 144, 500), (ResNet, 2.5, 250), (WaveletModel, 10, 100) ]:
-    for task in combinations_dict.keys():
-        for dat in combinations_dict[task]:
-            exp3 = Experiment(dat, SegmentBeats, freq, sec, model, task, 'inter', 100, episodes=False)
+for model, sec, freq in [(CNN, 0.72, 360),(CPSCWinnerNet, 0.72, 500), (ResNet, 0.72, 250),(RTACNN, 0.72, 300)]: #(CNN, 10, 360), (RTACNN, 30, 300), (CPSCWinnerNet, 144, 500), (ResNet, 2.5, 250), (WaveletModel, 10, 100) ]:
+    for task in combinations_dict_beats.keys():
+        for dat in combinations_dict_beats[task]:
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'intra', 100, episodes=False)
             exp3.run()
             exp3.evaluate()
 
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'inter', 100, episodes=False)
+            exp3.run()
+            exp3.evaluate()
+
+
+exp3 = Experiment(dat, Transform, 500,  0.72, CPSCWinnerNet, 'aami', 'intra', 100, episodes=False)
+exp3.run()
+exp3.evaluate()
+
+
+for model, sec, freq in [(CNN, 10, 360),(CPSCWinnerNet, 10, 500), (ResNet, 10, 250),(RTACNN, 10, 300)]: #(CNN, 10, 360), (RTACNN, 30, 300), (CPSCWinnerNet, 144, 500), (ResNet, 2.5, 250), (WaveletModel, 10, 100) ]:
+    for task in combinations_dict_episodes.keys():
+        for dat in combinations_dict_episodes[task]:
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'intra', 100, episodes=True, max_episode_seconds=sec)
+            exp3.run()
+            exp3.evaluate()
+            
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'inter', 100, episodes=True, max_episode_seconds=sec)
+            exp3.run()
+            exp3.evaluate()
+
+            if freq==300:
+                exp3 = Experiment(dat, SlidingWindow, freq, sec, model, task, 'intra', 100, episodes=True, max_episode_seconds=2.5)
+                exp3.run()
+                exp3.evaluate()
+                
+                exp3 = Experiment(dat, SlidingWindow, freq, sec, model, task, 'inter', 100, episodes=True, max_episode_seconds=2.5)
+                exp3.run()
+                exp3.evaluate()
+
+
+
+for model, sec, freq in [(CNN, 2.5, 360),(CPSCWinnerNet, 2.5, 500), (ResNet, 2.5, 250),(RTACNN, 2.5, 300)]: #(CNN, 10, 360), (RTACNN, 30, 300), (CPSCWinnerNet, 144, 500), (ResNet, 2.5, 250), (WaveletModel, 10, 100) ]:
+    for task in combinations_dict_episodes.keys():
+        for dat in combinations_dict_episodes[task]:
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'intra', 100, episodes=True, max_episode_seconds=sec)
+            exp3.run()
+            exp3.evaluate()
+
+            exp3 = Experiment(dat, Transform, freq, sec, model, task, 'inter', 100, episodes=True, max_episode_seconds=sec)
+            exp3.run()
+            exp3.evaluate()
