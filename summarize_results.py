cc8c5bb224e5e5467377b7b774f26c1182e28db0
elenamer <elenamerdzanovska@yahoo.com>
2021-12-09 22:55:59 +0100

    update summarization script

--- summarize_results.py
+++ summarize_results.py
@@ -1,20 +1,25 @@
 import os
 import argparse
+import math
 import numpy as np
 from numpy.lib import stride_tricks
 import pandas as pd
 import pickle
 
-tasks = ["form", "rhythm"]
+root = "experiments-marvin"
+tasks = ["aami"]
 
-classes = list(range(10))
 
 metrics = ["AUC","F1"]
-stats = ["mean", "var"]#,"std","min","max"]
-sets = ["test","val"]
+stats = ["mean"]#,"std","min","max"]
+sets = ["test"]#,"val"]
+evals = {'intra':['',os.sep+'intra'],
+'inter':[os.sep+'inter']
+}
 
-models=['cpscwinner', 'resnet', 'rtacnn','cnn','wavelet']
+models=  [d for d in os.listdir(root) if os.path.isdir(root+os.sep+d)] #['cpscwinner', 'resnet', 'rtacnn','cnn']
 models_and_all = models # just list them
+stats= list(evals.keys())
 
 models_ind = np.repeat(models_and_all, len(metrics)*len(stats) *len(sets))
 
@@ -32,54 +37,75 @@ sets_ind = sets * len(metrics) * len(models_and_all) * len(stats)
 datasets_ind = [] 
 segmentations_ind = []
 
-datasets = [d for d in os.listdir("experiments") if os.path.isdir("experiments"+os.sep+d)]
+datasets = [d for d in os.listdir(root) if os.path.isdir(root+os.sep+d)]
 
 print(datasets)
 
 for dataset in datasets:
-    segmentations = [d for d in os.listdir("experiments"+os.sep+dataset) if os.path.isdir("experiments"+os.sep+dataset+os.sep+d)]
+    segmentations = [d for d in os.listdir(root+os.sep+dataset) if os.path.isdir(root+os.sep+dataset+os.sep+d)]
     for segm in segmentations:
-        datasets_ind.append(dataset)
-        segmentations_ind.append(segm)
+        current_models = [m for m in os.listdir(root+os.sep+dataset+os.sep+segm) if os.path.isdir(root+os.sep+dataset+os.sep+segm+os.sep+m)]
+        for mod in current_models:
+            datasets_ind.append(mod)
+            segmentations_ind.append(segm)
 
 results = {}
 print(datasets_ind)
 print(segmentations_ind)
-
+print(stats_ind)
 results_df1 = pd.DataFrame(columns = [models_ind, metrics_ind, stats_ind, sets_ind], index=[datasets_ind, segmentations_ind] )
 results_df1 = results_df1.fillna('/')
 print(results_df1)
 
 for task in tasks:
 
+    print(task)
+
     results_df1 = pd.DataFrame(columns = [models_ind, metrics_ind, stats_ind, sets_ind], index=[datasets_ind, segmentations_ind] )
     results_df1 = results_df1.fillna('/')
-
-    for dataset in datasets:
-        segmentations = [d for d in os.listdir("experiments"+os.sep+dataset) if os.path.isdir("experiments"+os.sep+dataset+os.sep+d)]
-        for segm in segmentations:
-            models = [d for d in os.listdir("experiments"+os.sep+dataset+os.sep+segm) if os.path.isdir("experiments"+os.sep+dataset+os.sep+segm+os.sep+d)]
-            for model in models:
-                path = "experiments"+os.sep+dataset+os.sep+segm+os.sep+model+os.sep+task
-                print(path)
-                try:      
-                    with open(path+os.sep+'results.txt', 'r') as f:
-                        print(f)
-                        temp = f.read()
-                        dict = eval(temp)
-                        print(dict)
-                except:
-                    continue
-                for metric in metrics:
-                    for stat in stats:
-                        for dset in sets:
-                            print(dict[metric.lower()][dset.lower()])
-                            elem = dict[metric.lower()][dset.lower()][stat.lower()]
-                            results_df1.loc[(dataset, segm),(model, metric, stat, dset)] = elem
-    results[task]= results_df1
-
+    print(results_df1.index.names)
+    results_df1 = results_df1[~results_df1.index.duplicated(keep='first')]
+    for eval_p in evals.keys():
+        for dataset in datasets:
+            segmentations = [d for d in os.listdir(root+os.sep+dataset) if os.path.isdir(root+os.sep+dataset+os.sep+d)]
+            for segm in segmentations:
+                models = [d for d in os.listdir(root+os.sep+dataset+os.sep+segm) if os.path.isdir(root+os.sep+dataset+os.sep+segm+os.sep+d)]
+                for model in models:
+                    for opt in evals[eval_p]:
+                        path = root+os.sep+dataset+os.sep+segm+os.sep+model+os.sep+task+opt
+                        print(path)
+                        try:      
+                            with open(path+os.sep+'results.txt', 'r') as f:
+                                #print(f)
+                                temp = f.read()
+                                #print(temp)
+                                dict = eval(temp)
+                                #print(dict)
+                        except:
+                            continue
+                        for metric in metrics:
+                            for dset in sets:
+                                #for stat in stats:
+                                print(dict[metric.lower()][dset.lower()])
+                                elem = str(round(dict[metric.lower()][dset.lower()]['mean'], 4))+" ("+str(round(math.sqrt(dict[metric.lower()][dset.lower()]['var']), 4))+")"
+                                results_df1.loc[(model, segm),(dataset, metric, eval_p, dset)] = elem
+        results[(task, eval_p)]= results_df1
+print(results[(task, eval_p)])
 for task in tasks:
-    results[task].to_csv(os.path.join('.', "summary_"+task+".csv"))
+    for eval_p in evals.keys():
+        print(task, eval_p)
+        new_index= []
+        for row in results[(task, eval_p)].itertuples():
+            for el in row[1:]:
+                if el != '/':
+                    new_index.append(row[0])
+                    print(row[0])
+                    break
+        print(len(new_index))
+        print(new_index)
+        print(results[(task, eval_p)])
+        print(results[(task, eval_p)].loc[('cpscwinner', 'segmentepisodesthresholdstandard5000')])
+        results[(task, eval_p)].loc[new_index].sort_index(level=0).to_csv(os.path.join('.', "summary_"+task+"_"+eval_p+".csv"))
 
 
 def evaluate_metrics(confusion_matrix):
